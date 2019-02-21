4787b0d6277d1e08a4f83628c99dc6437db0824c
Guest User <Guest@epicodus-12.local>
2019-02-20 16:41:53 -0800

    init

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,13 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
+    
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
 //NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue())
+
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -29,6 +36,35 @@ namespace ToDoList.Tests
       //Assert
       Assert.AreEqual(description, result);
     }
-    
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,13 +1,17 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
 
     private string _description;
+    private static List<Item> _instances = new List<Item> {};
 
     public Item (string description)
     {
       _description = description;
+      _instances.Add(this);
     }
 
     public string GetDescription()
@@ -18,6 +22,16 @@ namespace ToDoList.Models
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
 
   }
